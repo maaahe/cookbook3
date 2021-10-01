8dc6d152dcc4b8c2faf572992aa7061f77f264f8
Martin Ahe <imartinahe@gmail.com>
2021-10-01 11:30:15 +0300

    built initial recipelist structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import RecipeEdit from "./components/RecipeEdit";
+import RecipeList from "./components/RecipeList";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RecipeList recipes={sampleRecipes}/>
+      <RecipeEdit />
+    </>
   );
 }
 
-export default App;
+const sampleRecipes = [
+  {
+    id: 1,
+    name: "Marinated strawberries",
+    cookTime: "1:45",
+    instructions: "1. Pick strawberry 2. Marinate strawberry 3. Eat strawberry",
+    ingredients: [{
+      ingredientName: "strawberry",
+      amount: 3
+    }, {
+      ingredientName: "jam",
+      amount: "2 scoops"
+    }]
+  },
+  {
+    id: 2,
+    name: "Roasted chocolate",
+    cookTime: "0:45",
+    instructions: "1. Roast chocolate 2. Eat strawberry",
+    ingredients: [{
+      ingredientName: "cacao beans",
+      amount: "1 kilo"
+    }, {
+      ingredientName: "chia seeds",
+      amount: "3 scoops"
+    }]
+  }]
