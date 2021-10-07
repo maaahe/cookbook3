52d5f5aeefd2929acd8a8bf17769ebbeb29ea687
Martin Ahe <imartinahe@gmail.com>
2021-10-07 11:02:45 +0300

    added a recipe component and passed props from app to ingredients through 4 layers

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,46 +1,25 @@
 import React from 'react'
-import Ingredients from './Ingredients'
+import Recipe from "./Recipe"
+
 
 export default function RecipeList({
     recipes,
     handleRecipeAdd,
     handleRecipeDelete,
-    showUuid
 }) {
     return (
         <>
             <div>
                 {recipes.map(recipe => 
-                    <>
-                        <div>
-                            <button>
-                                Edit
-                            </button>
-                            
-                            <button
-                                onClick={() => handleRecipeDelete(recipe)}
-                            >
-                                Delete
-                            </button>
-                        </div>
-                        <h1>{recipe.name}</h1>
-                        <div>
-                            <label>Cook time</label>
-                            <span>{recipe.cookTime}</span>
-                        </div>
-
-                        <div>
-                            <label>Instructions</label>
-                            <span>{recipe.instructions}</span>
-                        </div>
-
-                        <div>
-                            <label>Ingredients</label>
-                            <Ingredients ingredients={recipe.ingredients}/>
-                        </div>
-                    </>
+                    <Recipe 
+                        recipe={recipe}
+                        {...recipe}
+                        handleRecipeDelete={handleRecipeDelete}
+                    />
                 )}
             </div>
+
+            
 
             <button onClick={handleRecipeAdd}>Add recipe</button>
         </>
